1f95971821e400d1e8b4424144c68d92abe2e15b
N6SYNTAX <127181966+N6SYNTAX@users.noreply.github.com>
2025-05-15 20:27:40 +1000

    wk9

---
+++ Submissions/Submission9/Task_9_1/Task_4_2/IHaveInventory.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace SwinAdventure
+{
+    public interface IHaveInventory
+    {
+
+        GameObject Locate(string id);
+
+        string Name { get; }
+    }
+}
