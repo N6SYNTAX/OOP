437b3177b1b95713e3fe9358bcc599dfa6fd7716
N6SYNTAX <127181966+N6SYNTAX@users.noreply.github.com>
2025-06-08 12:25:48 +1000

    COMPLETE

---
+++ Submissions/CustProj/MyRectangle.cs
@@ -0,0 +1,82 @@
+using SplashKitSDK;
+using System;
+using System.IO;
+
+namespace Task_4
+{
+    public class MyRectangle : Shape
+    {
+        private int _width;
+        private int _height;
+
+
+        public MyRectangle() : this(SplashKit.ColorGreen(), 0.0f, 0.0f, 100 + 69, 100 + 69)
+        {
+        }
+
+        // Constructor with parameters.
+        public MyRectangle(Color color, float x, float y, int width, int height)
+            : base(color)
+        {
+            X = x;
+            Y = y;
+            _width = width;
+            _height = height;
+        }
+
+        // Public properties for width and height.
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+
+        public override void Draw()
+        {
+
+
+
+            SplashKit.FillRectangle(Color, X - (_width / 2.0f), Y - (_height / 2.0f), _width, _height);
+            if (Selected)
+            {
+                DrawOutline();
+            }
+        }
+
+
+        public override void DrawOutline()
+        {
+            int extra = 5;
+            SplashKit.DrawRectangle(SplashKit.ColorBlack(), X - extra, Y - extra, _width + 2 * extra, _height + 2 * extra);
+        }
+
+        public override void SaveTo(StreamWriter writer)
+        {
+            writer.WriteLine("Rectangle");
+            base.SaveTo(writer);
+            writer.WriteLine(_width);
+            writer.WriteLine(_height);
+        }
+
+        public override void LoadFrom(StreamReader reader)
+        {
+            base.LoadFrom(reader);
+            _width = reader.ReadInteger();
+            _height = reader.ReadInteger();
+        }
+
+
+        public override bool IsAt(Point2D pt)
+        {
+            return pt.X >= X && pt.X <= (X + _width) &&
+                   pt.Y >= Y && pt.Y <= (Y + _height);
+        }
+    }
+}
---
+++ Submissions/Task_3.1/Task_3_1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Task_3_1;
+
+namespace Task_3_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            IdentifiableObject id = new IdentifiableObject(new string[] { "007", "James", "Bond" });
+
+        }
+    }
+}
